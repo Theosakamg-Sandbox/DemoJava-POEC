95239190660f4c7777a23b825f9bd79e7ba827f5
Mickael Gaillard <mickael.gaillard@gmail.com>
2023-02-02 14:07:00 +0100

    Add Parking implementation
    Signed-off-by: Mickael Gaillard <mickael.gaillard@gmail.com>

--- src/com/epsi/mickael/tp2/Parking.java
+++ src/com/epsi/mickael/tp2/Parking.java
@@ -5,13 +5,31 @@ import java.util.ArrayList;
 public class Parking {
 
     protected int maxPlace = 3;
-    protected ArrayList<Car> storage = new ArrayList<>();
+    private ArrayList<Car> storage = new ArrayList<>();
 
+    // Déposer voiture
     public String put(Car car) {
-        return null;
+        if (this.storage.size() < this.maxPlace)
+            this.storage.add(car);
+
+        return car.getHash();
     }
 
+    // Récupérer une voiture.
     public Car pop(String ticket) {
-        return null;
+        Car result = null;
+
+        for (Car car : this.storage) {
+            if (ticket.equals(car.getHash())) {
+                result = car;
+                break; // Optim !
+            }
+        }
+
+        if (result != null) {
+            this.storage.remove(result);
+        }
+
+        return result;
     }
 }
